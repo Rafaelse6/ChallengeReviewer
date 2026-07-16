e8b194cb08d11ed25d3664a2a7b10f917d720cdc
Rafael Santos <rafa.05125@gmail.com>
2026-07-15 22:42:09 -0300

    Review model

---
+++ ChallengeReviewer.Core/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace ChallengeReviewer.Core.Models
+{
+    public sealed record Review
+    {
+        public int OverallScore { get; set; }
+        public string Grade { get; set; } = "";
+        public string Summary { get; set; } = "";
+        public string Encouragement { get; set; } = "";
+        public List<string> Strenghts { get; set; } = [];
+        public List<string> Improvements { get; set; } = [];
+        public List<string> CodeExamples { get; set; } = [];
+        public List<string> NextSteps { get; set; } = [];
+    }
+}
