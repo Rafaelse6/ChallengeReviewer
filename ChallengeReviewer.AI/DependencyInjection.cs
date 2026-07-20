aa28cf84118bdf7bdb73a8dabe391be010484490
Rafael Santos <rafa.05125@gmail.com>
2026-07-20 17:56:51 -0300

    DependencyInjection

---
+++ ChallengeReviewer.AI/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using ChallengeReviewer.AI.Agents;
+using ChallengeReviewer.Core.Agents.Abstractions;
+using ChallengeReviewer.Core.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChallengeReviewer.AI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAgents(this IServiceCollection services)
+        {
+            // -> Used for generic agents: services.AddKeyedTransient<IAgent<StaticAnalysisReport, Review>, Agent<StaticAnalysisReport, Review>>("ChallengeReviewer");
+            services.AddTransient<IAgent<StaticAnalysisReport, Review>, ChallengeReviewerAgent>();
+            return services;
+        }
+    }
+}
