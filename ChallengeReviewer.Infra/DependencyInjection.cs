ee70784d984785fb99be959d2b7277615be14cb2
Rafael Santos <rafa.05125@gmail.com>
2026-07-20 11:05:17 -0300

    DependencyInjection

---
+++ ChallengeReviewer.Infra/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using ChallengeReviewer.Core.Services.Abstractions;
+using ChallengeReviewer.Infra.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChallengeReviewer.Infra
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IGitHubService, GitHubService>();
+            return services;
+        }
+    }
+}
