562c72bde6f96f1b2c68586bdef60bde4f9178fd
Rafael Santos <rafa.05125@gmail.com>
2026-07-20 17:51:06 -0300

    ChallengeReviewerAgent

---
+++ ChallengeReviewer.AI/Agents/ChallengeReviewerAgent.cs
@@ -0,0 +1,29 @@
+﻿using ChallengeReviewer.Core.Enums;
+using ChallengeReviewer.Core.Models;
+
+namespace ChallengeReviewer.AI.Agents
+{
+    public class ChallengeReviewerAgent()
+        : Agent<StaticAnalysisReport, Review>(Name, Description, Prompt, Temperature, Instructions)
+    {
+        private const float Temperature = 0.7f;
+        private const string Name = "Challenge Reviewer";
+        private const string Description =
+            "Você é um agente especializado em gerar relatórios técnicos detalhados com base em análises estáticas de código";
+        private const string Instructions =
+            "você é um agente especialista em gerar relatórios técnicos detalhados com base em análises estáticas de código."
+            + "Use os dados fornecdiso para criar um relatório que destaque os principais pontos, "
+            + "incluindo métricas, problemas encontrados e sugestões de melhoria."
+            + "O relatório deve ser claro, conciso e fácil de entender para desenvolvedores e gerentes de projeto.";
+
+        private const string Prompt = "Gere um relatório técnico detalhado com base nestes dados: ";
+
+        //public override async Task<Review> RunAsync(
+        //    StaticAnalysisReport data, 
+        //    Provider provider, Model model, 
+        //    CancellationToken cancellationToken)
+        //{
+        //    return await base.RunAsync(data, provider, model, cancellationToken);
+        //}
+    }
+}
